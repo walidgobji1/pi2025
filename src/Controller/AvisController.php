f8e5cfc58c77a194f8c3c8f6a9198e47769aca06
amenibenabdallah <benabdallah2ameni@gmail.com>
2025-02-12 15:06:53 +0100

    Implémentation de la fonctionnalité d'ajout pour l'entité Avis avec Ajax

--- src/Controller/AvisController.php
+++ src/Controller/AvisController.php
@@ -3,10 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Avis;
+use App\Entity\Formation;
+use App\Entity\Apprenant;
+
+
 use App\Form\AvisType;
 use App\Repository\AvisRepository;
+use Doctrine\ORM\EntityManager;
+
+
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,40 +23,71 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AvisController extends AbstractController
 {
     #[Route(name: 'app_avis_index', methods: ['GET'])]
-    public function index(AvisRepository $avisRepository): Response
+    public function index(AvisRepository $avisRepository, EntityManagerInterface $entityManager): Response
     {
+        $avis = $entityManager->getRepository(Avis::class)->findAll();
+
+        // Get formationId and userId dynamically, this is temporary for now
+        $formationId = 1; // This will be dynamic later
+        $userId = 1; // Get the user ID from authentication
+
         return $this->render('avis/index.html.twig', [
-            'avis' => $avisRepository->findAll(),
+            'avis' => $avis,
+            'formationId' => $formationId, // Temporary for now
+            'userId' => $userId,           // Get dynamic user ID from the authentication system
         ]);
     }
 
-    #[Route('/new', name: 'app_avis_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $avi = new Avis();
-        $form = $this->createForm(AvisType::class, $avi);
-        $form->handleRequest($request);
+    #[Route('/add/{formationId}/{userId}', name: 'app_avis_add', methods: ['GET', 'POST'])]
+public function addAvis(int $formationId, int $userId, Request $request, EntityManagerInterface $entityManager): Response
+{
+    // Fetch the user and formation from the database
+    $userById = $entityManager->getRepository(Apprenant::class)->find($userId);
+    $formation = $entityManager->getRepository(Formation::class)->find($formationId);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+    if (!$userById || !$formation) {
+        // If the formation or user doesn't exist, return an error page or redirect
+        return $this->render('error.html.twig', [
+            'message' => 'Utilisateur ou formation non trouvé !'
+        ]);
+    }
+
+    // Create the Avis entity and set it up
+    $avi = new Avis();
+    $avi->setApprenant($userById);
+    $avi->setFormation($formation);
+
+    // Create and handle the form
+    $form = $this->createForm(AvisType::class, $avi);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        try {
             $entityManager->persist($avi);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_avis_index', [], Response::HTTP_SEE_OTHER);
+            return $this->json([
+                'status' => 'success',
+                'message' => 'Avis ajouté avec succès',
+            ]);
+        } catch (\Exception $e) {
+            return $this->json([
+                'status' => 'error',
+                'message' => 'Une erreur est survenue lors de l\'ajout de l\'avis.',
+            ]);
         }
-
-        return $this->render('avis/new.html.twig', [
-            'avi' => $avi,
-            'form' => $form,
-        ]);
     }
 
-    #[Route('/{id}', name: 'app_avis_show', methods: ['GET'])]
-    public function show(Avis $avi): Response
-    {
-        return $this->render('avis/show.html.twig', [
-            'avi' => $avi,
-        ]);
-    }
+    // On GET request, render the form page
+    return $this->render('avis/new.html.twig', [
+        'form' => $form->createView(),
+        'formationId' => $formationId,
+        'userId' => $userId,
+    ]);
+}
+
+    
+
 
     #[Route('/{id}/edit', name: 'app_avis_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Avis $avi, EntityManagerInterface $entityManager): Response
@@ -71,7 +110,7 @@ final class AvisController extends AbstractController
     #[Route('/{id}', name: 'app_avis_delete', methods: ['POST'])]
     public function delete(Request $request, Avis $avi, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$avi->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $avi->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($avi);
             $entityManager->flush();
         }
--- src/Form/AvisType.php
+++ src/Form/AvisType.php
@@ -2,52 +2,46 @@
 
 namespace App\Form;
 
-use App\Entity\Apprenant;
 use App\Entity\Avis;
-use App\Entity\Formation;
-use App\Entity\FormationScore;
-use App\Entity\Instructeur;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class AvisType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('note')
-            ->add('commentaire')
-            ->add('dateCreation', null, [
-                'widget' => 'single_text',
+            ->add('note', IntegerType::class, [
+                'label' => 'Note',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez donner une note.']),
+                    new Range([
+                        'min' => 1,
+                        'max' => 5,
+                        'notInRangeMessage' => 'La note doit être entre {{ min }} et {{ max }}.',
+                    ]),
+                ],
             ])
-            ->add('updateDate', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('formationScore', EntityType::class, [
-                'class' => FormationScore::class,
-                'choice_label' => 'id',
-            ])
-            ->add('instructeur', EntityType::class, [
-                'class' => Instructeur::class,
-                'choice_label' => 'id',
-            ])
-            ->add('apprenant', EntityType::class, [
-                'class' => Apprenant::class,
-                'choice_label' => 'id',
-            ])
-            ->add('formation', EntityType::class, [
-                'class' => Formation::class,
-                'choice_label' => 'id',
-            ])
-        ;
+            ->add('commentaire', TextareaType::class, [
+                'label' => 'Commentaire',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez ajouter un commentaire.']),
+                ],
+            ]);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => Avis::class,
+            'csrf_protection' => false, // Disable CSRF protection for this form
         ]);
     }
 }
