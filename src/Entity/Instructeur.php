ec03bf07b9131af2be43caadf5e73db02a69a3f5
Yassminette5 <yassminehnainia1@gmail.com>
2025-02-20 04:01:10 +0100

    resolve conflits

--- src/Entity/Instructeur.php
+++ src/Entity/Instructeur.php
@@ -2,137 +2,20 @@
 
 namespace App\Entity;
 
-use App\Repository\InstructeurRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: InstructeurRepository::class)]
-class Instructeur
+#[ORM\Entity]
+class Instructeur extends User
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(type: "string", nullable: true)]
+    private ?string $cv = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $CV = null;
+    #[ORM\Column(type: "string", nullable: true)]
+    private ?string $image = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $niveau = null;
+    public function getCv(): ?string { return $this->cv; }
+    public function setCv(?string $cv): self { $this->cv = $cv; return $this; }
 
-    #[ORM\Column(nullable: true)]
-    private ?float $scoreEvaluation = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?float $scoreAvis = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?bool $status = null;
-
-    /**
-     * @var Collection<int, Avis>
-     */
-    #[ORM\OneToMany(targetEntity: Avis::class, mappedBy: 'instructeur')]
-    private Collection $avis;
-
-    public function __construct()
-    {
-        $this->avis = new ArrayCollection();
-    }
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getCV(): ?string
-    {
-        return $this->CV;
-    }
-
-    public function setCV(?string $CV): static
-    {
-        $this->CV = $CV;
-
-        return $this;
-    }
-
-    public function getNiveau(): ?string
-    {
-        return $this->niveau;
-    }
-
-    public function setNiveau(?string $niveau): static
-    {
-        $this->niveau = $niveau;
-
-        return $this;
-    }
-
-    public function getScoreEvaluation(): ?float
-    {
-        return $this->scoreEvaluation;
-    }
-
-    public function setScoreEvaluation(?float $scoreEvaluation): static
-    {
-        $this->scoreEvaluation = $scoreEvaluation;
-
-        return $this;
-    }
-
-    public function getScoreAvis(): ?float
-    {
-        return $this->scoreAvis;
-    }
-
-    public function setScoreAvis(?float $scoreAvis): static
-    {
-        $this->scoreAvis = $scoreAvis;
-
-        return $this;
-    }
-
-    public function isStatus(): ?bool
-    {
-        return $this->status;
-    }
-
-    public function setStatus(?bool $status): static
-    {
-        $this->status = $status;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Avis>
-     */
-    public function getAvis(): Collection
-    {
-        return $this->avis;
-    }
-
-    public function addAvi(Avis $avi): static
-    {
-        if (!$this->avis->contains($avi)) {
-            $this->avis->add($avi);
-            $avi->setInstructeur($this);
-        }
-
-        return $this;
-    }
-
-    public function removeAvi(Avis $avi): static
-    {
-        if ($this->avis->removeElement($avi)) {
-            // set the owning side to null (unless already changed)
-            if ($avi->getInstructeur() === $this) {
-                $avi->setInstructeur(null);
-            }
-        }
-
-        return $this;
-    }
+    public function getImage(): ?string { return $this->image; }
+    public function setImage(?string $image): self { $this->image = $image; return $this; }
 }
