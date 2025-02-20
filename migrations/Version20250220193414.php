d543f848b8a0d420430992a15aec4c9e088e769c
ahmed0199 <ahmedallaya@gmail.com>
2025-02-20 23:10:15 +0100

    crud + Controle saisie

--- migrations/Version20250220193414.php
+++ migrations/Version20250220193414.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250217000906 extends AbstractMigration
+final class Version20250220193414 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,12 @@ final class Version20250217000906 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE discussion DROP created_at');
-        $this->addSql('ALTER TABLE utilisateur DROP nom');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1D1C63B3F85E0677 ON utilisateur (username)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE discussion ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE utilisateur ADD nom VARCHAR(255) NOT NULL');
+        $this->addSql('DROP INDEX UNIQ_1D1C63B3F85E0677 ON utilisateur');
     }
 }
